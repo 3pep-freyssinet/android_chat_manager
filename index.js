c64129010d8eac8f42683e88cd38bdaa5a427117
aa <aa@dot.com>
2024-06-01 16:41:49 +0100

    ejs works locally

--- index.js
+++ index.js
@@ -256,7 +256,21 @@ app
     };
 	
 	const response = 'Hello World from express listening on ' + port;
-    res.send(response);
+    //res.send(response);
+	
+	var mascots = [
+		{ name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+		{ name: 'Tux', organization: "Linux", birth_year: 1996},
+		{ name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+	];
+	var tagline = "No programming concept is complete without a cute animal mascot.";
+
+	res.render('pages/index', {
+		mascots: mascots,
+		tagline: tagline
+	});
+	
+	//res.render('pages/index');
 	
 	const fileName = 'index.html';
 	//res.sendFile(__dirname, 'index.html');
@@ -271,6 +285,10 @@ app
     });
 	*/
   })
+  // about page
+ .get('/about', function(req, res) {
+	res.render('pages/about');
+ });
   
  
 
